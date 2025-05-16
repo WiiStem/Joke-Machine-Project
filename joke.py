118d362833acb36b4170570cf754c8a6cea00681
Your Name <you@example.com>
2025-05-16 09:58:24 -0400

    intial commit

---
+++ joke.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+import requests
+
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/joke")
+def joke():
+    joke = None
+    api_url = "https://icanhazdadjoke.com/"
+    mood=["Happy", "Sad", "Stressed", "Bored"]
+    error = None
+    dad_joke=""
+    dad_jokes =[]
+    if request.method == "POST":
+        headers={"Accept":"application/json"}
+        response = requests.get(api_url,headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            dad_jokes = list(data["message"].keys())
+            dad_jokes.sort()
+            dad_joke = request.form.get("joke")
+    
+    return render_template("joke.html", mood=mood, dad_joke=dad_joke, dad_jokes=dad_jokes)
+
+@app.route("/search")
+def search():
+    search = None
+
+
+    return render_template("search.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
