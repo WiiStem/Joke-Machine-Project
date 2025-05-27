dc1820fe90460c36c505224ee75ac26790555043
Your Name <you@example.com>
2025-05-27 09:06:25 -0400

    joke template

--- joke.py
+++ joke.py
@@ -26,12 +26,21 @@ def joke():
     
     return render_template("joke.html", mood=mood, dad_joke=dad_joke, dad_jokes=dad_jokes)
 
-@app.route("/search")
-def search():
-    search = None
-
-
-    return render_template("search.html")
-
+@app.route("/search", methods=["GET", "POST"])
+def search_joke():
+        joke=None
+        term=None
+        error=None
+        jokes=""
+        if request.method == "POST":
+                term = request.form.get("search")
+                api_url = f"https://icanhazdadjoke.com/search?term={term}"
+                headers = {"Accept": "application/json"}
+                response = requests.get(api_url, headers=headers)
+                if response.status_code == 200:
+                        jokes = response.json().get("results")
+                else:
+                     error="Couldnt get your joke, either try again with a different term or try again later "
+        return render_template("search.html",jokes=jokes,error=error,joke=joke)
 if __name__ == "__main__":
     app.run(debug=True)
